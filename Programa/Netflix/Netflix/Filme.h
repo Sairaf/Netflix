9c6023216ab33cd9c32d4fbd2b69ccd904215a01
Sairaf <abnergillet16@gmail.com>
2014-10-20 07:57:44 -0200

    Update Filme.h

--- Programa/Netflix/Netflix/Filme.h
+++ Programa/Netflix/Netflix/Filme.h
@@ -1,3 +1,47 @@
+/*
+Filme.h 
+
+Função: Armazenar os atributos básicos de um filme
+
+Iniciar todos os atributos ok
+Iniciar 3 construtores (Default, cópia e um normal) ok
+Ter atributo string ok
+Ter atributo static ok
+Ter atributo const static ok
+Dois métodos constantes ok
+um array ok
+uma função inline (mensagem de boas vindas) ok
+método com passagem por referência ok
+Método static (chamado no main) ok
+
+Requisitos de implementação (2):
+
+Alocação dinâmica de memória.
+fried Operator<<
+Operator=
+vector push_back
+Requisitos de implementação (3):
+
+Duas classes. Classes devem possuir:
+Operator =
+Alocação dinâmica - se houver vazamento de memória a classe toda é desconsiderada
+Usar o destrutor
+Construtor de cópia
+Operator << friend
+Um const static
+
+
+
+
+
+
+
+
+
+*/
+
+
+
 #ifndef FILME_H
 #define FILME_H
 //========================
