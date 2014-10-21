880ab2bb375f939a296d612c9513553526b2bda6
Sairaf <abnergillet16@gmail.com>
2014-10-21 04:47:58 -0200

    Update Netflix.h

--- Programa/Netflix/Netflix/Netflix.h
+++ Programa/Netflix/Netflix/Netflix.h
@@ -1,3 +1,57 @@
+/*
+Função Netflix.h
+
+Criar pelo menos 4 atributos ok
+Criar pelo menos 4 funções (sem contar get e set) ok
+Iniciar todos os atributos ok
+Iniciar 3 construtores (Default, cópia e um normal) ok
+Ter atributo string ok
+Ter atributo static (Velocidade máxima?) ok
+Ter atributo const static (cnpj) ok
+Dois métodos constantes ok
+um array ok
+uma função inline (mensagem de boas vindas) ok
+método com passagem por referência ok
+Método static (chamado no main) ok
+composição com a classe Data ok
+
+Acréscimos
+
+Alocação dinâmica de memória. ok
+fried Operator<< ok
+Operator= ok
+vector push_back
+
+Atributos:
+cnpj : string - Armazena o cnpj
+existeUsuario: int - verifica se existe algum usuario
+velMaxima: static float - recebe o valor da velocidade maxima
+MENSALIDADE: const static float
+
+usuarioLogin: string - Recebe o login do usuário
+usuarioSenha: string - Recebe a senha do usuário
+tituloFilme: string* - Recebe um certo numero de filmes através da laocação dinâmica de memória
+data: Data - Recebe uma data colocada pelo usuário
+quantidadeFilmes: int - Recebe o numero atual de filmes
+
+MAXFILMES: const int - Numero maximo de iflmes que o usuario pode ter
+
+
+Métodos:
+Construtores de cópia,default e com passagem de parâmetros
+gets e sets para a maioria dos atributos
+
+const inline void Welcome - Escreve na tela uma pequena mensagem de boas vindas
+	  
+void AdicionarUsuario - Adiciona um usuário
+void AdicionarFilme - Adiciona um filme para a conta do usuario enquanto o atributo quantidadeFIlmes for menor que o MAXFILMES
+const void ListarUsuario - escreve o usuário e seus filmes
+float CalculoDownload(float, float) - Calcula a velocidade do download
+static float CalculoGanhos(int,const float) - calcula quanto seriam os ganhos com x usuarios
+Netflix operator=(const Netflix&) const - Sobrecarga de operador para =
+friend ostream &operator<<(ostream &, const Netflix &) : Método friend que auxilia a escrita dos atributos na tela.
+*/
+
 #include <iostream>
 #include <vector>
 #include <cstdlib>
@@ -21,7 +75,7 @@ private:
 	const static float MENSALIDADE;
 //============================================
 	string usuarioLogin;
-    string usuarioSenha;
+        string usuarioSenha;
 	string* tituloFilme;
 	static Data data;
 //==========================================	
@@ -33,7 +87,7 @@ public:
 	  
 	  Netflix(string usuarioLogin = "Default", string senha = "1rfdew");
 	  Netflix(string,string,string);
-      Netflix(const Netflix&);
+          Netflix(const Netflix&);
 	 ~Netflix();
 
       //setters
